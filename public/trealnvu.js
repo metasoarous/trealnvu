3df11e8577d9354a149d918bc0994d1e225f12dc
Christopher Small <csmall@fhcrc.org>
2012-06-24 15:29:08 -0700

    Looks like I may have forgotten to include the js file changes in last commit. Added readme also

--- public/trealnvu.js
+++ public/trealnvu.js
@@ -1,4 +1,3 @@
-
 //I'm thinking stick functions and library code in here, then call whatever relavant functions,
 //prototypes we contruct from a little script in the index.html to load the data in the data dir.
 //This has been set up to work so that if you make a request for /data/aligned.fasta, for example,
